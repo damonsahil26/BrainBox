8c6a6821bbc26f1eee590907cf2b70d15ed5c80f
Sahil Malhotra <sahil061626@gmail.com>
2024-10-20 11:36:14 +0530

    Scaffolded project and fixed login page

--- Models/Player.cs
+++ Models/Player.cs
@@ -4,6 +4,8 @@ namespace BrainBox.Models
 {
     public class Player : IdentityUser
     {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public int TotalMatchesPlayed { get; set; }
         public int TotalMatchesWon { get; set; }
     }
